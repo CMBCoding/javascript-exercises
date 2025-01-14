794d750abd0bfd77ad2fb6c01d2dcd1d4ad2bcfa
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-01-14 19:36:36 +0900

    Complete exercise two. Reverse string test function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    str.toString();
+    var reverse = str.split("");
+    var arrayReverse = reverse.reverse();
+    var arrayJoin = arrayReverse.join("");
+    return arrayJoin;
 };
 
 // Do not edit below this line
