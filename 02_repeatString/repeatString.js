27e4261fc6fd5806d0ad217c5879b9e278066c87
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-01-14 21:40:42 +0900

    Complete exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,10 @@
-const repeatString = function(phrase, round) {
-  if (round < 3) return "ERROR";
-  let str = "";
-  for (i = 0; i < round; i++) {
-    str += phrase;
-  }
-  return str;
-
+const repeatString = function(text, rounds) {
+    let str = "";
+    if (rounds < 0) return "ERROR";
+    for (let i = 0; i < rounds; i++) {
+        str = str + text;
+    }
+    return str;
 };
 
 // Do not edit below this line
