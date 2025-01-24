15ee4e651c02537bd5c59ac345b671495271c835
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-01-24 19:17:50 +0900

    Finish test two. Begin work on test three.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    // let start;
+    // let end;
+    // if (int1 > int2) {
+    //     end = int1;
+    //     start = int2;
+    //     return end, start;
+    // } else {
 
+    };
+    let sum = 0;
+    for (let i = int1; i <= int2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
