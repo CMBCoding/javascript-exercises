bcecf27d99cde2736f799bb1c2a1a135a40397d6
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-01-25 16:05:56 +0900

    Solve tests for conversion to C and F temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let C = (temp - 32) * (5/9);
+  return C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let F = (temp * (9/5)) + 32;
+  return F;
 };
 
 // Do not edit below this line
